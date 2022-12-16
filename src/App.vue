4248824ebbd48745b3c3da97b4c39a2fec619549
mihail <76650506+mihail666@users.noreply.github.com>
2022-12-16 15:12:09 +0500

    border
    b

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>inventory app</h1>
+  <i-inventoty />
+</template>
+
+<script>
+import IInventoty from './components/i-inventoty.vue'
+
+export default {
+  name: 'App',
+  components: {IInventoty}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
