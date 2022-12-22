dae8da147b9bc48ca1eab0b738e5017b0f2c3b06
mihail666 <madoneglhp@gmail.com>
2022-12-22 06:37:39 +0500

    new component

---
+++ src/components/inventory/i-inventory.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="inventory">
+      <div 
+        class="inventory__container container_wrapper borderBlack" 
+        :class="`${is_expTheme ? 'borderWhite' : ''}`"
+        >
+        <div class="inventory_grid">
+          <div 
+            v-for="item in items" 
+            :key="item.id" 
+            class="inventory__item menu-toggle-wrap">
+            <div class="inventory__item__content menu-toggle" @click="ToggleMenu">
+              <img src="@/assets/logo.png" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup(_, { emit }) {
+
+    const ToggleMenu = function () {
+      console.log('меню открыто')
+      emit('is_expended')
+    }
+    return {
+      ToggleMenu
+    }
+  },
+  data() {
+    return {
+      items: 25,
+    }
+  },
+  props: {
+    is_expTheme: {
+      type: Boolean,
+      default: true
+    }
+  }
+})
+</script>
+
+<style lang="scss">
+@import '@/assets/style/variable.scss';
+.inventory {
+  width: 100%;
+}
+
+.inventory__container {
+  display: flex;
+}
+
+.inventory_grid {
+  display: grid;
+  grid-gap: 0px;
+  grid-template-columns: repeat(auto-fill, 20%);
+  height: 100%;
+  width: 100%;
+  margin: auto;
+  justify-content: center;
+}
+
+.inventory__item {
+  height: 16vh;
+  position: relative;
+  list-style: none;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+
+  & img {
+    width: 60%;
+    background-size: contain;
+  }
+}
+
+.inventory__item:not(:nth-child(5n)) {
+  border-right: $border-int;
+}
+
+.inventory__item:not(:nth-last-child(-n)) {
+  border-bottom: $border-int ;
+}
+.inventory__item:nth-child(21),
+.inventory__item:nth-child(22),
+.inventory__item:nth-child(23),
+.inventory__item:nth-child(24),
+.inventory__item:nth-child(25) {
+  border-bottom: none ;
+}
+</style>
--- src/components/inventory/i-inventoty-wrapper.vue
+++ src/components/inventory/i-inventoty-wrapper.vue
@@ -4,23 +4,10 @@
       @is_expended_theme="isExpendedTheme" 
       :is_expTheme="is_expTheme"
     />
-    <div class="inventory">
-      <div 
-        class="inventory__container container_wrapper borderBlack" 
-        :class="`${is_expTheme ? 'borderWhite' : ''}`"
-        >
-        <div class="inventory_grid">
-          <div 
-            v-for="item in items" 
-            :key="item.id" 
-            class="inventory__item menu-toggle-wrap">
-            <div class="inventory__item__content menu-toggle" @click="ToggleMenu">
-              <img src="@/assets/logo.png" alt="">
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <i-inventory 
+      :is_expTheme="is_expTheme"
+      @is_expended="isExpanded"
+    />
   </div>
   <i-sidebar 
     :is_expanded="is_expanded" 
@@ -29,24 +16,25 @@
 </template>
 
 <script>
-import ISidebar from './i-sidebar.vue'
+import ISidebar from './i-sidebar.vue';
 import IMenubar from './i-menubar.vue';
+import IInventory from './i-inventory.vue';
 export default {
   components: {
     ISidebar,
-    IMenubar
+    IMenubar,
+    IInventory
   },
   data() {
     return {
-      items: 25,
       is_expTheme: false,
       is_expanded: true,
     }
   },
   methods: {
-    ToggleMenu() {
+    isExpanded() {
+      console.log('sidebar')
       this.is_expanded = !this.is_expanded;
-      console.log(this.is_expanded)
     },
     isExpendedTheme() {
       console.log('parent')
@@ -59,47 +47,4 @@ export default {
 <style lang="scss">
 @import '@/assets/style/variable.scss';
 
-
-
-.inventory {
-  width: 100%;
-}
-
-.inventory__container {
-  display: flex;
-
-}
-
-.inventory_grid {
-  display: grid;
-  grid-gap: 0px;
-  grid-template-columns: repeat(auto-fill, 20%);
-  height: 100%;
-  width: 100%;
-  margin: auto;
-  justify-content: center;
-}
-
-.inventory__item {
-  height: 16vh;
-  position: relative;
-  list-style: none;
-  display: flex;
-  align-items: center;
-  text-align: center;
-  justify-content: center;
-
-  & img {
-    width: 60%;
-    background-size: contain;
-  }
-}
-
-.inventory__item:not(:nth-child(5n)) {
-  border-right: $border-int;
-}
-
-.inventory__item:not(:nth-last-child(-n)) {
-  border-bottom: $border-int ;
-}
 </style>
